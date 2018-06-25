3d74058e7e031de69fb777475d7f688b45530851
Bruce Ricketts <byricketts98@gmail.com>
2018-06-25 19:22:59 -0400

    Add delete button to product list

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import * as R from 'ramda';
+
+const renderProduct = R.curry((component, product, index) => {
+  return (
+    <tr key={product.id}>
+      <td>{product.id}</td>
+      <td>{product.name}</td>
+      <td>{product.description}</td>
+      <td>{product.price}</td>
+      <td><button
+        onClick={e => {
+          e.preventDefault();
+          component.setState(R.over(R.lensProp('products'), R.drop(index)))
+        }}>
+        Delete
+      </button></td>
+    </tr>
+  );
+})
 
 export default class ProductList extends Component {
-  renderProduct({ id, name, description, price }) {
-    return (
-      <tr key={id}>
-        <td>{id}</td>
-        <td>{name}</td>
-        <td>{description}</td>
-        <td>{price}</td>
-      </tr>
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: props.items
+    }
   }
-  
+
   render() {
     return (
       <table>
@@ -22,7 +37,7 @@ export default class ProductList extends Component {
             <th>Description</th>
             <th>Price</th>
           </tr>
-          {this.props.items.map(this.renderProduct)}
+          {this.state.products.map(renderProduct(this))}
         </tbody>
       </table>
     );
